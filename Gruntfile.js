3ffa1fd809d52b18094f99812fb08dc4a28cb75a
theromanseasonal <ben@theromanseasonal.com>
2014-05-23 10:35:36 -0700

    Lap Around Grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+
+	grunt.initConfig({
+		clean: ['./dist'],
+		copy: {
+        	release: {
+            	files: [ { src: './**', dest: './dist/' } ]
+        	}
+    	}
+	});
+
+
+	require('load-grunt-tasks')(grunt);
+	grunt.registerTask('default', ['clean', 'copy']);
+
+};
