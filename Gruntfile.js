c24fbde1531899e69f4ac3af1625f801e2941bc8
theromanseasonal <ben@theromanseasonal.com>
2014-05-23 12:00:56 -0700

    adding gruntplugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,41 @@
 module.exports = function(grunt) {
 
+	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
 		clean: ['./dist'],
+
 		copy: {
         	release: {
             	files: [ { src: './**', dest: './dist/' } ]
         	}
-    	}
+    	},
+
+		version: {
+		    release: {
+		        options: {
+		            buildNumber: "1.0." + (+new Date())
+		        }
+		    },
+		    dev: {
+		        options: {
+		            buildNumber: function() {
+		                var package = JSON.parse(grunt.file.read('./package.json'));
+		                var chunks = package.version.split('.');
+		                chunks[1] = parseInt(chunks[1], 10) + 1;
+		                return chunks.join('.') + '-pre';
+		            }()
+		        }
+		    }
+		}
 	});
+	
+  	grunt.registerMultiTask('version', 'Add build number to the package.json', function() {
+  		var package = JSON.parse(grunt.file.read('./package.json'));
+		package.version = this.options().buildNumber;
 
+		grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
+  	});
 
-	require('load-grunt-tasks')(grunt);
-	grunt.registerTask('default', ['clean', 'copy']);
-
+	grunt.registerTask('default', ['clean', 'copy', 'version']);
 };
